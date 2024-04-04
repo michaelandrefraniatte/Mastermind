a59b92cd6f3acf489cea703e9eb188a0c6c99b49
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-04 23:24:21 +0200

    Initial commit

---
+++ Src/Mastermind/Mastermind/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Mastermind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbcolor1line1 = new System.Windows.Forms.PictureBox();
+            this.pbcolor2line1 = new System.Windows.Forms.PictureBox();
+            this.pbcolor3line1 = new System.Windows.Forms.PictureBox();
+            this.pbcolor4line1 = new System.Windows.Forms.PictureBox();
+            this.pbcheck1line1 = new System.Windows.Forms.PictureBox();
+            this.pbcheck2line1 = new System.Windows.Forms.PictureBox();
+            this.pbcheck3line1 = new System.Windows.Forms.PictureBox();
+            this.pbcheck4line1 = new System.Windows.Forms.PictureBox();
+            this.btcheckline1 = new System.Windows.Forms.Button();
+            this.pbcolor4linesee = new System.Windows.Forms.PictureBox();
+            this.pbcolor3linesee = new System.Windows.Forms.PictureBox();
+            this.pbcolor2linesee = new System.Windows.Forms.PictureBox();
+            this.pbcolor1linesee = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor1line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor2line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor3line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor4line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcheck1line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcheck2line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcheck3line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcheck4line1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor4linesee)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor3linesee)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor2linesee)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor1linesee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbcolor1line1
+            // 
+            this.pbcolor1line1.BackColor = System.Drawing.Color.Silver;
+            this.pbcolor1line1.Location = new System.Drawing.Point(127, 503);
+            this.pbcolor1line1.Name = "pbcolor1line1";
+            this.pbcolor1line1.Size = new System.Drawing.Size(50, 50);
+            this.pbcolor1line1.TabIndex = 0;
+            this.pbcolor1line1.TabStop = false;
+            // 
+            // pbcolor2line1
+            // 
+            this.pbcolor2line1.BackColor = System.Drawing.Color.Silver;
+            this.pbcolor2line1.Location = new System.Drawing.Point(219, 503);
+            this.pbcolor2line1.Name = "pbcolor2line1";
+            this.pbcolor2line1.Size = new System.Drawing.Size(50, 50);
+            this.pbcolor2line1.TabIndex = 1;
+            this.pbcolor2line1.TabStop = false;
+            // 
+            // pbcolor3line1
+            // 
+            this.pbcolor3line1.BackColor = System.Drawing.Color.Silver;
+            this.pbcolor3line1.Location = new System.Drawing.Point(305, 503);
+            this.pbcolor3line1.Name = "pbcolor3line1";
+            this.pbcolor3line1.Size = new System.Drawing.Size(50, 50);
+            this.pbcolor3line1.TabIndex = 2;
+            this.pbcolor3line1.TabStop = false;
+            // 
+            // pbcolor4line1
+            // 
+            this.pbcolor4line1.BackColor = System.Drawing.Color.Silver;
+            this.pbcolor4line1.Location = new System.Drawing.Point(396, 503);
+            this.pbcolor4line1.Name = "pbcolor4line1";
+            this.pbcolor4line1.Size = new System.Drawing.Size(50, 50);
+            this.pbcolor4line1.TabIndex = 3;
+            this.pbcolor4line1.TabStop = false;
+            // 
+            // pbcheck1line1
+            // 
+            this.pbcheck1line1.BackColor = System.Drawing.Color.Silver;
+            this.pbcheck1line1.Location = new System.Drawing.Point(25, 503);
+            this.pbcheck1line1.Name = "pbcheck1line1";
+            this.pbcheck1line1.Size = new System.Drawing.Size(20, 20);
+            this.pbcheck1line1.TabIndex = 4;
+            this.pbcheck1line1.TabStop = false;
+            // 
+            // pbcheck2line1
+            // 
+            this.pbcheck2line1.BackColor = System.Drawing.Color.Silver;
+            this.pbcheck2line1.Location = new System.Drawing.Point(62, 503);
+            this.pbcheck2line1.Name = "pbcheck2line1";
+            this.pbcheck2line1.Size = new System.Drawing.Size(20, 20);
+            this.pbcheck2line1.TabIndex = 5;
+            this.pbcheck2line1.TabStop = false;
+            // 
+            // pbcheck3line1
+            // 
+            this.pbcheck3line1.BackColor = System.Drawing.Color.Silver;
+            this.pbcheck3line1.Location = new System.Drawing.Point(25, 533);
+            this.pbcheck3line1.Name = "pbcheck3line1";
+            this.pbcheck3line1.Size = new System.Drawing.Size(20, 20);
+            this.pbcheck3line1.TabIndex = 6;
+            this.pbcheck3line1.TabStop = false;
+            // 
+            // pbcheck4line1
+            // 
+            this.pbcheck4line1.BackColor = System.Drawing.Color.Silver;
+            this.pbcheck4line1.Location = new System.Drawing.Point(62, 533);
+            this.pbcheck4line1.Name = "pbcheck4line1";
+            this.pbcheck4line1.Size = new System.Drawing.Size(20, 20);
+            this.pbcheck4line1.TabIndex = 7;
+            this.pbcheck4line1.TabStop = false;
+            // 
+            // btcheckline1
+            // 
+            this.btcheckline1.BackColor = System.Drawing.Color.White;
+            this.btcheckline1.Location = new System.Drawing.Point(494, 503);
+            this.btcheckline1.Name = "btcheckline1";
+            this.btcheckline1.Size = new System.Drawing.Size(67, 50);
+            this.btcheckline1.TabIndex = 8;
+            this.btcheckline1.Text = "Check";
+            this.btcheckline1.UseVisualStyleBackColor = false;
+            // 
+            // pbcolor4linesee
+            // 
+            this.pbcolor4linesee.BackColor = System.Drawing.Color.Silver;
+            this.pbcolor4linesee.Location = new System.Drawing.Point(396, 12);
+            this.pbcolor4linesee.Name = "pbcolor4linesee";
+            this.pbcolor4linesee.Size = new System.Drawing.Size(50, 50);
+            this.pbcolor4linesee.TabIndex = 12;
+            this.pbcolor4linesee.TabStop = false;
+            // 
+            // pbcolor3linesee
+            // 
+            this.pbcolor3linesee.BackColor = System.Drawing.Color.Silver;
+            this.pbcolor3linesee.Location = new System.Drawing.Point(305, 12);
+            this.pbcolor3linesee.Name = "pbcolor3linesee";
+            this.pbcolor3linesee.Size = new System.Drawing.Size(50, 50);
+            this.pbcolor3linesee.TabIndex = 11;
+            this.pbcolor3linesee.TabStop = false;
+            // 
+            // pbcolor2linesee
+            // 
+            this.pbcolor2linesee.BackColor = System.Drawing.Color.Silver;
+            this.pbcolor2linesee.Location = new System.Drawing.Point(219, 12);
+            this.pbcolor2linesee.Name = "pbcolor2linesee";
+            this.pbcolor2linesee.Size = new System.Drawing.Size(50, 50);
+            this.pbcolor2linesee.TabIndex = 10;
+            this.pbcolor2linesee.TabStop = false;
+            // 
+            // pbcolor1linesee
+            // 
+            this.pbcolor1linesee.BackColor = System.Drawing.Color.Silver;
+            this.pbcolor1linesee.Location = new System.Drawing.Point(127, 12);
+            this.pbcolor1linesee.Name = "pbcolor1linesee";
+            this.pbcolor1linesee.Size = new System.Drawing.Size(50, 50);
+            this.pbcolor1linesee.TabIndex = 9;
+            this.pbcolor1linesee.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(494, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 50);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "See";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Mastermind.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(591, 565);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pbcolor4linesee);
+            this.Controls.Add(this.pbcolor3linesee);
+            this.Controls.Add(this.pbcolor2linesee);
+            this.Controls.Add(this.pbcolor1linesee);
+            this.Controls.Add(this.btcheckline1);
+            this.Controls.Add(this.pbcheck4line1);
+            this.Controls.Add(this.pbcheck3line1);
+            this.Controls.Add(this.pbcheck2line1);
+            this.Controls.Add(this.pbcheck1line1);
+            this.Controls.Add(this.pbcolor4line1);
+            this.Controls.Add(this.pbcolor3line1);
+            this.Controls.Add(this.pbcolor2line1);
+            this.Controls.Add(this.pbcolor1line1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mastermind";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor1line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor2line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor3line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor4line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcheck1line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcheck2line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcheck3line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcheck4line1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor4linesee)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor3linesee)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor2linesee)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcolor1linesee)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbcolor1line1;
+        private System.Windows.Forms.PictureBox pbcolor2line1;
+        private System.Windows.Forms.PictureBox pbcolor3line1;
+        private System.Windows.Forms.PictureBox pbcolor4line1;
+        private System.Windows.Forms.PictureBox pbcheck1line1;
+        private System.Windows.Forms.PictureBox pbcheck2line1;
+        private System.Windows.Forms.PictureBox pbcheck3line1;
+        private System.Windows.Forms.PictureBox pbcheck4line1;
+        private System.Windows.Forms.Button btcheckline1;
+        private System.Windows.Forms.PictureBox pbcolor4linesee;
+        private System.Windows.Forms.PictureBox pbcolor3linesee;
+        private System.Windows.Forms.PictureBox pbcolor2linesee;
+        private System.Windows.Forms.PictureBox pbcolor1linesee;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Src/Mastermind/Mastermind/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mastermind
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static Region rg;
+        private static GraphicsPath gp;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gp = new GraphicsPath();
+            gp.AddEllipse(pbcolor1linesee.DisplayRectangle);
+            rg = new Region(gp);
+            pbcolor1linesee.Region = rg;
+            pbcolor2linesee.Region = rg;
+            pbcolor3linesee.Region = rg;
+            pbcolor4linesee.Region = rg;
+            pbcolor1line1.Region = rg;
+            pbcolor2line1.Region = rg;
+            pbcolor3line1.Region = rg;
+            pbcolor4line1.Region = rg;
+            gp = new GraphicsPath();
+            gp.AddEllipse(pbcheck1line1.DisplayRectangle);
+            rg = new Region(gp);
+            pbcheck1line1.Region = rg;
+            pbcheck2line1.Region = rg;
+            pbcheck3line1.Region = rg;
+            pbcheck4line1.Region = rg;
+        }
+    }
+}
